1896f92f8db0771d532e22a2a4cf5ac9128bd2d3
selvanayaki91 <selvarjunan91@gmail.com>
2023-02-04 18:44:00 -0500

    Second commit

---
+++ Java-learn/src/com/pratical/Second.java
@@ -0,0 +1,14 @@
+package com.pratical;
+
+public class Second {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+
+
+	}
+
+}
