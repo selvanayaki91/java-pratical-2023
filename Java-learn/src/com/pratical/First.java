196b2c632eb508b84b67bae3cc135c6b9ebb2fdb
selvanayaki91 <selvarjunan91@gmail.com>
2023-02-21 01:54:01 -0500

    commited

--- Java-learn/src/com/pratical/First.java
+++ Java-learn/src/com/pratical/First.java
@@ -4,6 +4,9 @@ public class First {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		
 		System.out.println("Welcome to Java");
 		System.out.println("Welcome to Java");
 		System.out.println("Welcome to Java");
